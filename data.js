e9b89e7533e5f74242ede9dfb4415690997a008b
klst999 <mahamadouparita9999@gmail.com>
2025-02-17 12:39:49 +0000

    data.js

--- data.js
+++ data.js
@@ -1,151 +1,127 @@
 const offres = [    
   {
-    titre: 'Logistics Supervisor, Warehouse',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14781'
+    titre: "Operateur de Grue",
+    entreprise: "Fekola SA",
+        lieu: "Mali",
+    diplome: "",
+    datePublication: "15 Février 2025",
+    dateFin: "25 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/833b9c51-3979-45e2-8536-48589945f286/Fekola%20SA.png",
+    lien: "https://malibaara.com/jobs/details/14833"
   },
   {
-    titre: 'Logistics Coordinator, Dispatch',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14780'
+    titre: "Contrôleur de coûts - Projet",
+    entreprise: "Une Société de la place",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "14 Février 2025",
+    dateFin: "20 Février 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/14831"
   },
   {
-    titre: 'O&M Coordinator, Site Security',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14779'
+    titre: "Un (e) (01) Assistante (e) Suivi Evaluation Redevabilité et Apprentissage",
+    entreprise: "Croix-Rouge Malienne",
+        lieu: "Mopti",
+    diplome: "Bac+3",
+    datePublication: "13 Février 2025",
+    dateFin: "24 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/cd9f44f5-5855-45fb-ab26-14339bb9af9d/logo%20CRM.png",
+    lien: "https://malibaara.com/jobs/details/14830"
   },
   {
-    titre: 'O&M Coordinator, Data Entry',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14778'
+    titre: "Un (01) Responsable Auditeur interne",
+    entreprise: "Une Société de la place",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "12 Février 2025",
+    dateFin: "26 Février 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/14829"
   },
   {
-    titre: 'Civil Engineer',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14777'
+    titre: "PMO Manager",
+    entreprise: "IENG GROUP",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "12 Février 2025",
+    dateFin: "26 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
+    lien: "https://malibaara.com/jobs/details/14828"
   },
   {
-    titre: 'Support officer',
-    entreprise: 'IENG GROUP',
-    lieu: "Mali",
-         salaire : "200 000 fcfa - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '11 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg',
-    lien: 'https://malibaara.com/jobs/details/14776'
+    titre: "UN(E) PHARMACIEN(NE) DISPENSATEUR",
+    entreprise: "CSLS-TBH",
+        lieu: "Bamako",
+    diplome: "Bac+8",
+    datePublication: "11 Février 2025",
+    dateFin: "28 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/e95eaa48-b06d-461b-bc99-241b474a9da2/CLCS.png",
+    lien: "https://malibaara.com/jobs/details/14827"
   },
   {
-    titre: 'Agent.e de projet – LUTTE CONTRE LA DETENTION PROVISOIRE ILLEGALE AU MALI',
-    entreprise: 'Avocats sans frontières CANADA',
-    lieu: "Bamako",
-         salaire : "200 000 fca - 800 000 fcfa",
-        diplome: "Bac+3",
-    datePublication: '28 Janvier 2025',
-    dateFin: '21 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/86b5cf0b-616c-4d65-bda2-5fc87a00e9c5/logo%20avsf.png',
-    lien: 'https://malibaara.com/jobs/details/14775'
+    titre: "DEUX (02) MÉDECINS (MEDECIN/COORDINATEUR",
+    entreprise: "CSLS-TBH",
+        lieu: "Bamako",
+    diplome: "Bac+8",
+    datePublication: "11 Février 2025",
+    dateFin: "28 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/e95eaa48-b06d-461b-bc99-241b474a9da2/CLCS.png",
+    lien: "https://malibaara.com/jobs/details/14826"
   },
   {
-    titre: 'e-opérateurs de storefront',
-    entreprise: 'ABM INDUSTRIE SARL',
-    lieu: "Bamako",
-         salaire : "",
-        diplome: "",
-    datePublication: '27 Janvier 2025',
-    dateFin: '14 Février 2025',
-    logo: 'https://malibaara.com/assets/images/default-job.png',
-    lien: 'https://malibaara.com/jobs/details/14772'
+    titre: "AGENT COMMERCIAL",
+    entreprise: "Une Société de la place",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "10 Février 2025",
+    dateFin: "6 Mars 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/14825"
   },
   {
-    titre: 'PROGRAMME DE FORMATION DU MOIS DE FEVRIER 2025',
-    entreprise: 'Mining and Safety services',
-    lieu: "Bamako",
-         salaire : "",
-        diplome: "",
-    datePublication: '22 Janvier 2025',
-    dateFin: '12 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/c1eaaeb3-376b-49db-b20e-42f24540911c/PHOTO-2022-03-17-08-11-28.jpg',
-    lien: 'https://malibaara.com/jobs/details/14761'
-  },
-    {
-    titre: 'FORMATION EN SUIVI-EVALUTION (initiation/perfectionnement)',
-    entreprise: 'Cabinet SERRA',
-    lieu: "Bamako",
-         salaire : "",
-        diplome: "Bac+2",
-    datePublication: '17 Janvier 2025',
-    dateFin: '28 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/f94a8050-cb82-4e14-bde9-0de40daae715/serra.png',
-    lien: 'https://malibaara.com/jobs/details/14755'
+    titre: "QHSE Supervisor",
+    entreprise: "IENG GROUP",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "7 Février 2025",
+    dateFin: "24 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
+    lien: "https://malibaara.com/jobs/details/14824"
   },
   {
-    titre: 'PROGRAMME DE FORMATION EN GESTION DE LA PAIE',
-    entreprise: 'Cabinet SERRA',
-    lieu: "Bamako",
-         salaire : "",
-        diplome: "Bac+2",
-    datePublication: '17 Janvier 2025',
-    dateFin: '28 Février 2025',
-    logo: 'https://malibaara-assets.s3.eu-west-3.amazonaws.com/f94a8050-cb82-4e14-bde9-0de40daae715/serra.png',
-    lien: 'https://malibaara.com/jobs/details/14754'
-  },
+    titre: "QHSE Coordinator",
+    entreprise: "IENG GROUP",
+        lieu: "Bamako",
+    diplome: "Bac+3",
+    datePublication: "7 Février 2025",
+    dateFin: "24 Février 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
+    lien: "https://malibaara.com/jobs/details/14823"
+  }
 
 ];
 
-const currentDate = new Date();  // Date actuelle
+const mois = {
+  "Janvier": 0, "Février": 1, "Mars": 2, "Avril": 3, "Mai": 4, "Juin": 5,
+  "Juillet": 6, "Août": 7, "Septembre": 8, "Octobre": 9, "Novembre": 10, "Décembre": 11
+};
+
+// Fonction pour convertir une date en format 'dd Mois yyyy' en objet Date
+function convertirDate(dateStr) {
+  const [jour, moisNom, annee] = dateStr.split(' ');
+  return new Date(annee, mois[moisNom], jour);
+}
+
+const currentDate = new Date(); // Date actuelle
 
 offres.forEach(offre => {
-    // Convertir la date de fin de l'offre en objet Date pour la comparaison
-    const dateFin = new Date(offre.dateFin);
+    const dateFin = convertirDate(offre.dateFin);
 
-    // Créer l'élément pour l'offre et l'ajouter à la page
-    const offreElement = document.createElement('div');
-    offreElement.classList.add('offre-card');
+    if (currentDate <= dateFin) {  // On garde uniquement les offres non expirées
+        const offreElement = document.createElement('div');
+        offreElement.classList.add('offre-card');
 
-    // Vérifier si l'offre est expirée
-    if (currentDate > dateFin) {
-        offreElement.innerHTML = `
-            <img src="${offre.logo}" alt="Logo de ${offre.entreprise}" class="logo">
-            <h3>${offre.titre}</h3>
-            <p><strong>Entreprise :</strong> ${offre.entreprise}</p>
-            <p><strong>Lieu :</strong> ${offre.lieu}</p>
-            <p><strong>Diplôme :</strong> ${offre.diplome}</p>
-            <p><strong>Date de publication :</strong> ${offre.datePublication}</p>
-            <p><strong>Date de fin :</strong> ${offre.dateFin}</p>
-            <p><strong>Cette offre est expirée.</strong></p>
-        `;
-    } else {
         offreElement.innerHTML = `
             <img src="${offre.logo}" alt="Logo de ${offre.entreprise}" class="logo">
             <h3>${offre.titre}</h3>
@@ -156,8 +132,7 @@ offres.forEach(offre => {
             <p><strong>Date de fin :</strong> ${offre.dateFin}</p>
             <a href="${offre.lien}" target="_blank">Voir l'offre</a>
         `;
-    }
 
-    // Ajouter l'élément à la page
-    document.getElementById('offres').appendChild(offreElement);
+        document.getElementById('offres').appendChild(offreElement);
+    }
 });
