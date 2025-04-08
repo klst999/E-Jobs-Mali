2ae83d2f23e2b5e925ab80b1721a9a9aec3713c8
klst999 <mahamadouparita9999@gmail.com>
2025-04-08 13:38:09 +0000

    data.js

--- data.js
+++ data.js
@@ -1,105 +1,334 @@
 const offres = [    
   {
-    titre: "Operateur de Grue",
-    entreprise: "Fekola SA",
-        lieu: "Mali",
-    diplome: "",
-    datePublication: "15 Février 2025",
-    dateFin: "25 Février 2025",
-    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/833b9c51-3979-45e2-8536-48589945f286/Fekola%20SA.png",
-    lien: "https://malibaara.com/jobs/details/14833"
+    titre: "Director of Program Development and Quality",
+    entreprise: "Save the Children",
+    lieu:"Bamako",
+    diplome:"Bac+5",
+    datePublication: "8 Avril 2025",
+    dateFin: "29 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/fd27b72d-c21f-4405-877e-450a345f3956/Save%20the%20Children.png",
+    lien: "https://malibaara.com/jobs/details/15008"
   },
   {
-    titre: "Contrôleur de coûts - Projet",
-    entreprise: "Une Société de la place",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "14 Février 2025",
-    dateFin: "20 Février 2025",
-    logo: "https://malibaara.com/assets/images/default-job.png",
-    lien: "https://malibaara.com/jobs/details/14831"
+    titre: "MÉDECIN DU TRAVAIL",
+    entreprise: "Lithium du Mali SA",
+     lieu:"Gouloumina",
+    diplome:"Bac+8",
+    datePublication: "7 Avril 2025",
+    dateFin: "20 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/eefde4de-dc9c-4bcd-bc3c-a955afe5c7dd/Logo%20Lithuim.png",
+    lien: "https://malibaara.com/jobs/details/15007"
   },
   {
-    titre: "Un (e) (01) Assistante (e) Suivi Evaluation Redevabilité et Apprentissage",
+    titre: "UN(E) CHARGE(E) DE GESTION FINANCIERE",
+    entreprise: "Humanite et Inclusion",
+     lieu:"Bamako",
+    diplome:"Bac+4",
+    datePublication: "7 Avril 2025",
+    dateFin: "13 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/2ce9567b-c653-4757-8582-1abd2fb2c436/Humanite%20et%20Inclusion.png",
+    lien: "https://malibaara.com/jobs/details/15006"
+  },
+  {
+    titre: "Un Gestionnaire de Stocks",
+    entreprise: "AFRIK EMPLOI",
+     lieu:"Bamako",
+    diplome:"Bac+3,Bac+4",
+    datePublication: "5 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+    lien: "https://malibaara.com/jobs/details/15005"
+  },
+  {
+    titre: "Un Contrôleur Qualité",
+    entreprise: "AFRIK EMPLOI",
+     lieu:"Bamako",
+    diplome:"Bac+3",
+    datePublication: "5 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+    lien: "https://malibaara.com/jobs/details/15004"
+  },
+  {
+    titre: "Superviseur Mécanicien Forage",
+    entreprise: "SAER EMPLOI",
+     lieu:"Kenieba",
+    diplome:"Bac+3",
+    datePublication: "4 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/00933e81-dfaa-4e34-8864-785037464ef9/SAER%20EMPLOI.png",
+    lien: "https://malibaara.com/jobs/details/15003"
+  },
+  {
+    titre: "Mécanicien et Electricien engins lourds",
+    entreprise: "SAER EMPLOI",
+        lieu:"Kenieba",
+    diplome:"BT2",
+    datePublication: "4 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/00933e81-dfaa-4e34-8864-785037464ef9/SAER%20EMPLOI.png",
+    lien: "https://malibaara.com/jobs/details/15002"
+  },
+  {
+    titre: "Six (06) Animateurs (trices)",
     entreprise: "Croix-Rouge Malienne",
-        lieu: "Mopti",
-    diplome: "Bac+3",
-    datePublication: "13 Février 2025",
-    dateFin: "24 Février 2025",
+        lieu:"Bamako, Kayes et Gao",
+    diplome:"Bac+3",
+    datePublication: "4 Avril 2025",
+    dateFin: "11 Avril 2025",
     logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/cd9f44f5-5855-45fb-ab26-14339bb9af9d/logo%20CRM.png",
-    lien: "https://malibaara.com/jobs/details/14830"
+    lien: "https://malibaara.com/jobs/details/15000"
   },
   {
-    titre: "Un (01) Responsable Auditeur interne",
+    titre: "Deux (02) Comptable Terrain",
+    entreprise: "Croix-Rouge Malienne",
+        lieu:"Kayes et Mopti",
+    diplome:"Bac+3",
+    datePublication: "4 Avril 2025",
+    dateFin: "11 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/cd9f44f5-5855-45fb-ab26-14339bb9af9d/logo%20CRM.png",
+    lien: "https://malibaara.com/jobs/details/14999"
+  },
+  {
+    titre: "Un (e) (01) Coordinateur (trice) de Projet Migration",
+    entreprise: "Croix-Rouge Malienne",
+        lieu:"Bamako",
+    diplome:"Bac+3,Bac+4",
+    datePublication: "4 Avril 2025",
+    dateFin: "15 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/cd9f44f5-5855-45fb-ab26-14339bb9af9d/logo%20CRM.png",
+    lien: "https://malibaara.com/jobs/details/14998"
+  },
+  {
+    titre: "Un (e) (01) Spécialiste (Action Anticipatoire) (AA)",
+    entreprise: "Croix-Rouge Malienne",
+        lieu:"Bamako",
+    diplome:"Bac+4",
+    datePublication: "4 Avril 2025",
+    dateFin: "15 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/cd9f44f5-5855-45fb-ab26-14339bb9af9d/logo%20CRM.png",
+    lien: "https://malibaara.com/jobs/details/14997"
+  },
+  {
+    titre: "Une Responsable Commerciale",
     entreprise: "Une Société de la place",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "12 Février 2025",
-    dateFin: "26 Février 2025",
+        lieu:"Bamako",
+    diplome:"Bac+3,Bac+5",
+    datePublication: "4 Avril 2025",
+    dateFin: "7 Mai 2025",
     logo: "https://malibaara.com/assets/images/default-job.png",
-    lien: "https://malibaara.com/jobs/details/14829"
+    lien: "https://malibaara.com/jobs/details/14995"
   },
   {
-    titre: "PMO Manager",
+    titre: "HR Specialist",
     entreprise: "IENG GROUP",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "12 Février 2025",
-    dateFin: "26 Février 2025",
+        lieu:"Bamako",
+    diplome:"Bac+3",
+    datePublication: "3 Avril 2025",
+    dateFin: "10 Avril 2025",
     logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
-    lien: "https://malibaara.com/jobs/details/14828"
+    lien: "https://malibaara.com/jobs/details/14991"
   },
   {
-    titre: "UN(E) PHARMACIEN(NE) DISPENSATEUR",
-    entreprise: "CSLS-TBH",
-        lieu: "Bamako",
-    diplome: "Bac+8",
-    datePublication: "11 Février 2025",
-    dateFin: "28 Février 2025",
-    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/e95eaa48-b06d-461b-bc99-241b474a9da2/CLCS.png",
-    lien: "https://malibaara.com/jobs/details/14827"
+    titre: "Assistant technique partenariat du secteur privé",
+    entreprise: "LuxDev",
+        lieu:"Kayes, Koulikoro, Sikasso et Bamako",
+    diplome:"Bac+5",
+    datePublication: "3 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/f2105479-03ee-474e-98af-9224f8b09ee6/rr.png",
+    lien: "https://malibaara.com/jobs/details/14990"
   },
   {
-    titre: "DEUX (02) MÉDECINS (MEDECIN/COORDINATEUR",
-    entreprise: "CSLS-TBH",
-        lieu: "Bamako",
-    diplome: "Bac+8",
-    datePublication: "11 Février 2025",
-    dateFin: "28 Février 2025",
-    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/e95eaa48-b06d-461b-bc99-241b474a9da2/CLCS.png",
-    lien: "https://malibaara.com/jobs/details/14826"
+    titre: "Un Technicien de Ligne",
+    entreprise: "AFRIK EMPLOI",
+        lieu:"Bamako",
+    diplome:"Bac+2",
+    datePublication: "2 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+    lien: "https://malibaara.com/jobs/details/14989"
   },
   {
-    titre: "AGENT COMMERCIAL",
-    entreprise: "Une Société de la place",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "10 Février 2025",
-    dateFin: "6 Mars 2025",
-    logo: "https://malibaara.com/assets/images/default-job.png",
-    lien: "https://malibaara.com/jobs/details/14825"
+    titre: "Un Opérateur",
+    entreprise: "AFRIK EMPLOI",
+        lieu:"Bamako",
+    diplome:"BEP,BT",
+    datePublication: "2 Avril 2025",
+    dateFin: "18 Avril 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+    lien: "https://malibaara.com/jobs/details/14988"
   },
   {
-    titre: "QHSE Supervisor",
-    entreprise: "IENG GROUP",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "7 Février 2025",
-    dateFin: "24 Février 2025",
-    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
-    lien: "https://malibaara.com/jobs/details/14824"
-  },
-  {
-    titre: "QHSE Coordinator",
-    entreprise: "IENG GROUP",
-        lieu: "Bamako",
-    diplome: "Bac+3",
-    datePublication: "7 Février 2025",
-    dateFin: "24 Février 2025",
-    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/3b735171-5edb-458d-9533-4a8e6960130b/1571306680049.jpg",
-    lien: "https://malibaara.com/jobs/details/14823"
-  }
-
+  titre: "Un Opérateur 16/PDP (Presse)",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BEP,BT",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14987"
+},
+{
+  titre: "Un Opérateur CIL",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BEP,BT",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14986"
+},
+{
+  titre: "Un Opérateur Cellophaneuse",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamko",
+    diplome:"BEP,BT",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14985"
+},
+{
+  titre: "Un Manutentionnaire",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BFEM,BT2",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14984"
+},
+{
+  titre: "Un Magasinier",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+2,Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14983"
+},
+{
+  titre: "Un Infirmier",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14982"
+},
+{
+  titre: "Un Conducteur Charriot",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Certificat de Cariste",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14979"
+},
+{
+  titre: "Un Comptable",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14978"
+},
+{
+  titre: "Un Chef d'Equipe",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BT,BTS",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14977"
+},
+{
+  titre: "Un Barquetteur/Opérateur SP98",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BEP ,BT",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14976"
+},
+{
+  titre: "Une Assistante de Direction",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamko",
+    diplome:"BTS",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14975"
+},
+{
+  titre: "Un Assistant RH",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamko",
+    diplome:"Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14974"
+},
+{
+  titre: "Un Assistant QHSE",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14973"
+},
+{
+  titre: "Un Assistant IT",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+3",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14972"
+},
+{
+  titre: "Aide Magasinier",
+  entreprise: "AFRIK EMPLOI",
+      lieu:"Bamako",
+    diplome:"BTS",
+  datePublication: "2 Avril 2025",
+  dateFin: "18 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c0ce9091-95c1-4ac5-8462-7e201bdfe4ed/AFRIK%20EMPLOI-2.png",
+  lien: "https://malibaara.com/jobs/details/14971"
+},
+{
+  titre: "Cuisinier Personnel",
+  entreprise: "UMO Mali",
+      lieu:"Bamako",
+    diplome:"",
+  datePublication: "2 Avril 2025",
+  dateFin: "9 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/13df3294-7401-4626-882b-3cd0346bedb7/UMO%20Mali.png",
+  lien: "https://malibaara.com/jobs/details/14970"
+},
+{
+  titre: "Responsable Marketing Digital",
+  entreprise: "SAER EMPLOI",
+      lieu:"Bamako",
+    diplome:"Bac+4",
+  datePublication: "2 Avril 2025",
+  dateFin: "14 Avril 2025",
+  logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/00933e81-dfaa-4e34-8864-785037464ef9/SAER%20EMPLOI.png",
+  lien: "https://malibaara.com/jobs/details/14969"
+}
 ];
 
 const mois = {
