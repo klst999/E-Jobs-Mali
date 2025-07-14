320dbd181670a100d979018c56056d4292a67904
klst999 <mahamadouparita9999@gmail.com>
2025-07-14 11:32:37 +0000

    data.js

--- data.js
+++ data.js
@@ -1,4 +1,314 @@
 const offres = [    
+     {
+    titre: "Officier de formation - Spécialiste de la formation informatique",
+    entreprise: "Fekola SA",
+    lieu: "Bamako",
+    diplome: "Bac+3 en Informatique ou équivalent",
+    datePublication: "14 Juillet 2025",
+    dateFin: "21 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/833b9c51-3979-45e2-8536-48589945f286/Fekola%20SA.png",
+    lien: "https://malibaara.com/jobs/details/15264"
+  },
+  {
+    titre: "Spéciale bourse d’études LMD de grade Licence et Master",
+    entreprise: "IDH Consulting Group",
+    lieu: "Bamako",
+    diplome: "Baccalauréat",
+    datePublication: "12 Juillet 2025",
+    dateFin: "31 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/5128aea1-2579-4575-8a3a-61c4afa5e08a/IDH%20International.png",
+    lien: "https://malibaara.com/jobs/details/15263"
+  },
+  {
+    titre: "FORMATION CERTIFIANTE EN : GESTION DE PROJETS D’ENTREPRISES ET DE DÉVELOPPEMENT",
+    entreprise: "IDH Consulting Group",
+    lieu: "Bamako",
+    diplome: "Non précisé",
+    datePublication: "12 Juillet 2025",
+    dateFin: "31 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/5128aea1-2579-4575-8a3a-61c4afa5e08a/IDH%20International.png",
+    lien: "https://malibaara.com/jobs/details/15262"
+  },
+  {
+    titre: "Un (01) Surintendant Mécanique",
+    entreprise: "SEMOS SA",
+    lieu: "Kayes",
+    diplome: "Bac+5 en Génie Mécanique",
+    datePublication: "11 Juillet 2025",
+    dateFin: "5 Aout 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/0d5307c9-acb8-4b9d-b87d-cf1340e04308/SEMOS%20SA.png",
+    lien: "https://malibaara.com/jobs/details/15261"
+  },
+  {
+    titre: "Stagiaire Commercial(e)",
+    entreprise: "UMO Mali",
+    lieu: "Bamako",
+    diplome: "Bac+2 en Commerce ou Gestion",
+    datePublication: "10 Juillet 2025",
+    dateFin: "24 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/13df3294-7401-4626-882b-3cd0346bedb7/UMO%20Mali.png",
+    lien: "https://malibaara.com/jobs/details/15258"
+  },
+  {
+    titre: "Un (01) Surintendant de la Géologie",
+    entreprise: "SEMOS SA",
+    lieu: "Kayes",
+    diplome: "Bac+5 en Géologie",
+    datePublication: "9 Juillet 2025",
+    dateFin: "21 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/0d5307c9-acb8-4b9d-b87d-cf1340e04308/SEMOS%20SA.png",
+    lien: "https://malibaara.com/jobs/details/15256"
+  },
+  {
+    titre: "COORDINATEUR DOUANE (SITE DE BOUGOUNI)",
+    entreprise: "Lithium du Mali SA",
+    lieu: "Bougouni",
+    diplome: "Bac+3 en Commerce International ou Douane",
+    datePublication: "9 Juillet 2025",
+    dateFin: "23 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/eefde4de-dc9c-4bcd-bc3c-a955afe5c7dd/Logo%20Lithuim.png",
+    lien: "https://malibaara.com/jobs/details/15255"
+  },
+  {
+    titre: "COMPTABLE PROGRAMME",
+    entreprise: "Humanite et Inclusion",
+    lieu: "Bamako",
+    diplome: "Bac+3 en Comptabilité",
+    datePublication: "9 Juillet 2025",
+    dateFin: "16 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/2ce9567b-c653-4757-8582-1abd2fb2c436/Humanite%20et%20Inclusion.png",
+    lien: "https://malibaara.com/jobs/details/15254"
+  },
+  {
+    titre: "Security Manager",
+    entreprise: "FHI360",
+    lieu: "Bamako",
+    diplome: "Bac+4 Sécurité / Gestion de Risques",
+    datePublication: "9 Juillet 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/2a7459f7-39e2-4bc6-9d1f-4e8110a10753/LOGO-FHI-360-NEW.png",
+    lien: "https://malibaara.com/jobs/details/15253"
+  },
+  {
+    titre: "Project Manager – Spécialiste Agro-finance",
+    entreprise: "RMCR",
+    lieu: "Bamako",
+    diplome: "Bac+4 en Finance ou Agriculture",
+    datePublication: "8 Juillet 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/96906c20-09ea-4a61-af87-6e6011bad3e2/reseau-de-micro-institutions-de-croissance-et-de-revenus-rmcr-161898.jpg",
+    lien: "https://malibaara.com/jobs/details/15252"
+  },
+  {
+    titre: "Formation sur Les modalités de résiliation des contrats de travail et leurs répercussions juridiques en droit malien",
+    entreprise: "Malibaara",
+    lieu: "Bamako",
+    diplome: "Tous niveaux acceptés",
+    datePublication: "8 Juillet 2025",
+    dateFin: "30 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/6563aa41-8cbd-4884-878b-3e915e286db4/logo-transparent-Malibaara%20(6).png",
+    lien: "https://malibaara.com/jobs/details/15251"
+  },
+  {
+    titre: "Un (e) Manager Women Protection Empowerment",
+    entreprise: "International Rescue Committee",
+    lieu: "Bamako",
+    diplome: "Bac+3 Action Humanitaire ou Social",
+    datePublication: "8 Juillet 2025",
+    dateFin: "17 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/9d45aa87-cb76-4372-a888-664300166c2d/logo%20irc.png",
+    lien: "https://malibaara.com/jobs/details/15250"
+  },
+  {
+    titre: "Un (1) Sales Engineer - Mechanical / Mineral Processing (Mining Sector)",
+    entreprise: "Une Société de la place",
+    lieu: "Bamako",
+    diplome: "Ingénieur Mécanique ou Procédés Minier",
+    datePublication: "7 Juillet 2025",
+    dateFin: "17 Juillet 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15246"
+  },
+  {
+    titre: "Une Commerciale dynamique et ambitieuse",
+    entreprise: "Une Société de la place",
+    lieu: "Bamako",
+    diplome: "Bac+2 Commerce ou Marketing",
+    datePublication: "7 Juillet 2025",
+    dateFin: "20 Juillet 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15245"
+  },
+  {
+    titre: "STAGIAIRES COMMERCIAUX (H/F)",
+    entreprise: "Une Société de la place",
+    lieu: "Bamako",
+    diplome: "Bac ou plus",
+    datePublication: "7 Juillet 2025",
+    dateFin: "28 Juillet 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15244"
+  },
+  {
+    titre: "Surintendant Energie et Electricité",
+    entreprise: "SOMISY",
+    lieu: "Yanfolila",
+    diplome: "Ingénieur électricité ou énergie",
+    datePublication: "7 Juillet 2025",
+    dateFin: "16 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/2d4d5e0b-3d1d-4e21-927a-4ff72a2ea0e4/unnamed%20(6).png",
+    lien: "https://malibaara.com/jobs/details/15243"
+  },
+  {
+    titre: "Un (01) Géologue Senior de Cartographie",
+    entreprise: "SEMOS SA",
+    lieu: "Kéniéba",
+    diplome: "Ingénieur Géologue Senior",
+    datePublication: "4 Juillet 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/0d5307c9-acb8-4b9d-b87d-cf1340e04308/SEMOS%20SA.png",
+    lien: "https://malibaara.com/jobs/details/15242"
+  },
+  {
+    titre: "Un Ingénieur Sénior chargé de planification à long terme",
+    entreprise: "SEMOS SA",
+    lieu: "Kéniéba",
+    diplome: "Ingénieur des Mines ou équivalent",
+    datePublication: "4 Juillet 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/0d5307c9-acb8-4b9d-b87d-cf1340e04308/SEMOS%20SA.png",
+    lien: "https://malibaara.com/jobs/details/15241"
+  },
+  {
+    titre: "Responsable Administratif(ve) et Financier(ère)",
+    entreprise: "SAER EMPLOI",
+    lieu: "Bamako",
+    diplome: "Bac+4 Gestion ou Finance",
+    datePublication: "4 Juillet 2025",
+    dateFin: "17 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/00933e81-dfaa-4e34-8864-785037464ef9/SAER%20EMPLOI.png",
+    lien: "https://malibaara.com/jobs/details/15240"
+  },
+  {
+    titre: "Un(e) Responsable du Marketing et de la Communication",
+    entreprise: "Un établissement d’enseignement supérieur privé",
+    lieu: "Bamako",
+    diplome: "Bac+3 Marketing ou Communication",
+    datePublication: "4 Juillet 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15239"
+  },
+  {
+    titre: "Un (e) Vétérinaire 2",
+    entreprise: "Comité international de la Croix-Rouge (CICR)",
+    lieu: "Bamako",
+    diplome: "Docteur en Médecine Vétérinaire",
+    datePublication: "4 Juillet 2025",
+    dateFin: "17 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/53063cd6-69ff-4fc7-b1bc-22f48596fc77/Comit%C3%A9%20international%20de%20la%20Croix-Rouge%20(CICR).png",
+    lien: "https://malibaara.com/jobs/details/15238"
+  },
+  {
+    titre: "Technicien de Surface (Cleaner)",
+    entreprise: "International Medical Corps (IMC)",
+    lieu: "Bamako",
+    diplome: "Niveau primaire ou plus",
+    datePublication: "4 Juillet 2025",
+    dateFin: "10 Aout 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/62bd7c01-912a-4d8a-8ee4-3bf5485a3073/International%20Medical%20Corps%20(IMC).png",
+    lien: "https://malibaara.com/jobs/details/15237"
+  },
+  {
+    titre: "Coordonnateur Mobilisation des Ressources",
+    entreprise: "Fundación Educación y Cooperación (Educo)",
+    lieu: "Bamako",
+    diplome: "Bac+4 ou équivalent",
+    datePublication: "2 Juillet 2025",
+    dateFin: "31 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/5bd764cc-a8a5-42b5-85d3-2cde6fa1c7bb/unnamed%20(7).png",
+    lien: "https://malibaara.com/jobs/details/15234"
+  },
+  {
+    titre: "PROGRAMME DE FORMATION DU MOIS DE JUILLET 2025",
+    entreprise: "Mining and Safety services",
+    lieu: "Bamako",
+    diplome: "Tous niveaux",
+    datePublication: "1 Juillet 2025",
+    dateFin: "22 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/c1eaaeb3-376b-49db-b20e-42f24540911c/PHOTO-2022-03-17-08-11-28.jpg",
+    lien: "https://malibaara.com/jobs/details/15230"
+  },
+  {
+    titre: "Distributeur Mobile Money",
+    entreprise: "UMO Mali",
+    lieu: "Bamako",
+    diplome: "Niveau BEPC ou équivalent",
+    datePublication: "30 Juin 2025",
+    dateFin: "25 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/13df3294-7401-4626-882b-3cd0346bedb7/UMO%20Mali.png",
+    lien: "https://malibaara.com/jobs/details/15229"
+  },
+  {
+    titre: "Chef Comptable",
+    entreprise: "UMO Mali",
+    lieu: "Bamako",
+    diplome: "Bac+4 Comptabilité/Finance",
+    datePublication: "25 Juin 2025",
+    dateFin: "16 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/13df3294-7401-4626-882b-3cd0346bedb7/UMO%20Mali.png",
+    lien: "https://malibaara.com/jobs/details/15217"
+  },
+  {
+    titre: "FORMATION ANGLAIS DES AFFAIRES ET PROFESSIONNEL",
+    entreprise: "AMC CONSULTING’S GROUP",
+    lieu: "Bamako",
+    diplome: "Tous niveaux acceptés",
+    datePublication: "24 Juin 2025",
+    dateFin: "24 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/9d8bfcd9-9687-48fb-8b84-42a50ad85e89/AMG%20Consulting.jpg",
+    lien: "https://malibaara.com/jobs/details/15209"
+  },
+  {
+    titre: "Superviseur de Distribution",
+    entreprise: "UMO Mali",
+    lieu: "Bamako",
+    diplome: "Bac+2 Commerce ou Logistique",
+    datePublication: "20 Juin 2025",
+    dateFin: "20 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/13df3294-7401-4626-882b-3cd0346bedb7/UMO%20Mali.png",
+    lien: "https://malibaara.com/jobs/details/15207"
+  },
+  {
+    titre: "PROGRAMME DE FORMATION DE JUILLET 2025 POUR CERTIFICATION EN GESTION ET SUIVI-EVALUATION DU PROJET",
+    entreprise: "GLOBAL RH PLUS",
+    lieu: "Bamako",
+    diplome: "Tous niveaux acceptés",
+    datePublication: "19 Juin 2025",
+    dateFin: "19 Juillet 2025",
+    logo: "https://malibaara-assets.s3.eu-west-3.amazonaws.com/e2b346e4-2ad2-4fab-89ea-b8ca3f4e94f7/GLOBAL%20RH%20PLUS.png",
+    lien: "https://malibaara.com/jobs/details/15204"
+  },
+  {
+    titre: "Commercial(e)/Informaticien/ASSISTANT(E) ADMINISTRATIVE",
+    entreprise: "computech",
+    lieu: "Bamako",
+    diplome: "Bac+2 ou équivalent",
+    datePublication: "18 Juin 2025",
+    dateFin: "18 Juillet 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15201"
+  },
+  {
+    titre: "Commercial Fret et logistique",
+    entreprise: "EXPEDIMA TRANSIT",
+    lieu: "Bamako",
+    diplome: "Bac+2 ou plus en logistique ou commerce",
+    datePublication: "18 Juin 2025",
+    dateFin: "20 Aout 2025",
+    logo: "https://malibaara.com/assets/images/default-job.png",
+    lien: "https://malibaara.com/jobs/details/15200"
+  },
    {
     titre: "Responsable Achats et Logistique (RAL)",
     entreprise: "ARCAD SANTE PLUS",
